60b95dcaee1cec5541fda6388752119dabbade2b
dschawel <david.schawel@gmail.com>
2019-12-12 14:07:23 -0800

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Required node modules
+let express = require('express')
+
+// Declare express app variable
+let app = express()
+
+// Set up and middleware
+
+
+// Add any controllers
+
+// Add home or catch-all routes
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.listen(3000, () => {
+    console.log('Good morning!')
+}) 
