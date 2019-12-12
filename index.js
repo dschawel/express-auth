1ab342b202728913af198332df10d5db10a76ec8
dschawel <david.schawel@gmail.com>
2019-12-12 14:17:51 -0800

    Added work

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 // Required node modules
 let express = require('express')
+let layouts = require('express-ejs-layouts')
 
 // Declare express app variable
 let app = express()
 
 // Set up and middleware
-
+app.set('view engine', 'ejs')
+app.use(layouts)
 
 // Add any controllers
 
 // Add home or catch-all routes
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+    res.render('Home')
 })
 
 app.listen(3000, () => {
