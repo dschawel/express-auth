feee83ffbfa03ccd46d1493fa1640e339fdc1dd5
dschawel <david.schawel@gmail.com>
2020-01-03 14:51:04 -0800

    added Facebook oauth login

--- config/passportConfig.js
+++ config/passportConfig.js
@@ -1,5 +1,10 @@
+// Read ENV Values
+require('dotenv').config()
+
 // Require passport and any passport strategies you wish to use
 let passport = require('passport')
+let FacebookStrategy = require('passport-facebook').Strategy
+let GithubStrategy = require('passport-github2').Strategy
 let LocalStrategy = require('passport-local').Strategy
 
 // Reference the models folder to access the database
@@ -41,6 +46,78 @@ passport.use(new LocalStrategy({
         else {
             // Valid user and a valid password
             cb(null, foundUser)
+        }
+    })
+    .catch(cb)
+}))
+
+// Implement Github Strategy
+passport.use(new GithubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_SECRET,
+    callbackURL: process.env.BASE_URL + '/auth/callback/github'
+}, (accessToken, refreshToken, profile, cb) => {
+    let name = profile.displayName.split(' ')
+    db.user.findOrCreate({
+        where: { githubId: profile.id },
+        defaults: {
+            githubToken: accessToken,
+            firstname: name[0] || profile.username,
+            lastname: name[name.length - 1],
+            username: profile.username,
+            photoUrl: profile._json.avatar_url,
+            bio: profile._json.bio || `Github user ${profile.username} works at ${profile._json.company} in ${profile._json.location}`
+        }
+    })
+    .then(([user, wasCreated]) => {
+        return cb(null, user)
+    })
+    .catch(cb)
+}))
+
+// Implement Facebook Strategy
+passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.BASE_URL + '/auth/callback/facebook',
+    profileFields: ['id', 'email', 'displayName', 'photos', 'birthday']
+}, (accessToken, refreshToken, profile, cb) => {
+    console.log('Facebook login', profile._json.picture.data)
+    // Grab the Facebook primary email
+    let facebookEmail = profile.emails[0].value
+    let displayName = profile.displayName.split(' ')
+    let photo = profile.photos.length ? profile.photos[0].value : 
+    'https://res.cloudinary.com/briezh/image/upload/v1555956782/tg57atqguantflp2q2e5.jpg'
+
+    // Look for the email in the local database - DO NOT DUPLICATE!
+    db.user.findOrCreate({
+        where: { email: facebookEmail },
+        defaults: {
+            facebookToken: accessToken,
+            facebookId: profile.id,
+            firstname: displayName[0],
+            lastname: displayName[displayName.length - 1],
+            username: profile.username || displayName[0],
+            photoUrl: photo,
+            birthdate: profile._json.birthday,
+            bio: `${profile.displayName} created this account with Facebook!`
+        }
+    })
+    .then(([user, wasCreated]) => {
+        // Did we create a new user?
+        if (wasCreated  || user.facebookId) {
+            // New user, not found in local databse
+            cb(null, user)
+        } else {
+            // We found an existing user (add FB Id and Token)
+            user.update({
+                facebookId: profile.id,
+                facebookToken: accessToken
+            })
+            .then(updateUser => {
+                cb(null, updatedUser)
+            })
+            .catch(cb)
         }
     })
     .catch(cb)
